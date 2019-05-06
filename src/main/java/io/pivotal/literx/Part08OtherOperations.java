75fc0f46960fc23bcdaf2ee988e6c7661248af08
RRoggia <renanzr@gmail.com>
2019-05-06 16:39:35 -0300

    use tuple variation to ensure type safety

--- src/main/java/io/pivotal/literx/Part08OtherOperations.java
+++ src/main/java/io/pivotal/literx/Part08OtherOperations.java
@@ -14,13 +14,8 @@ public class Part08OtherOperations {
 	Flux<User> userFluxFromStringFlux(Flux<String> usernameFlux, Flux<String> firstnameFlux,
 			Flux<String> lastnameFlux) {
 
-		return Flux.zip(p -> {
-			String username = (String) p[0];
-			String firstName = (String) p[1];
-			String lastName = (String) p[2];
-			return new User(username, firstName, lastName);
-
-		}, usernameFlux, firstnameFlux, lastnameFlux);
+		return Flux.zip(usernameFlux, firstnameFlux, lastnameFlux)
+				.map(tuple -> new User(tuple.getT1(), tuple.getT2(), tuple.getT3()));
 	}
 
 	Mono<User> useFastestMono(Mono<User> mono1, Mono<User> mono2) {
