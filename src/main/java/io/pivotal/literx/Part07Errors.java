c38251c2246e26c9374d8917d80bc393b807e1d5
RRoggia <renanzr@gmail.com>
2019-05-06 16:04:27 -0300

    add solution to part error -part 7

--- src/main/java/io/pivotal/literx/Part07Errors.java
+++ src/main/java/io/pivotal/literx/Part07Errors.java
@@ -29,26 +29,24 @@ import reactor.core.publisher.Mono;
  */
 public class Part07Errors {
 
-//========================================================================================
-
-	// TODO Return a Mono<User> containing User.SAUL when an error occurs in the input Mono, else do not change the input Mono.
 	Mono<User> betterCallSaulForBogusMono(Mono<User> mono) {
-		return null;
+		return mono.onErrorReturn(User.SAUL);
 	}
 
-//========================================================================================
-
-	// TODO Return a Flux<User> containing User.SAUL and User.JESSE when an error occurs in the input Flux, else do not change the input Flux.
 	Flux<User> betterCallSaulAndJesseForBogusFlux(Flux<User> flux) {
-		return null;
+		return flux.onErrorResume(exception -> {
+			return Flux.just(User.SAUL, User.JESSE);
+		});
 	}
 
-//========================================================================================
-
-	// TODO Implement a method that capitalizes each user of the incoming flux using the
-	// #capitalizeUser method and emits an error containing a GetOutOfHereException error
 	Flux<User> capitalizeMany(Flux<User> flux) {
-		return null;
+		return flux.flatMap(user -> {
+			try {
+				return Mono.just(capitalizeUser(user));
+			} catch (GetOutOfHereException e1) {
+				return Flux.error(new GetOutOfHereException());
+			}
+		});
 	}
 
 	User capitalizeUser(User user) throws GetOutOfHereException {
